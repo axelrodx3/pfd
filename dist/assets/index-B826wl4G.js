ef1fc90c36a389e21bcc379bae4873af47b36406
axelrodx3 <axelrodx3@gmail.com>
2025-09-20 17:26:21 -0400

    Update build assets and fix deployment configuration

--- dist/assets/index-B826wl4G.js
+++
@@ -1,5 +0,0 @@
-import { g as r, r as s } from './index-BfPnbdt4.js'
-var o = s()
-const a = r(o)
-export { a as b }
-//# sourceMappingURL=index-B826wl4G.js.map
---
+++ dist/assets/index-fXNRe4Cl.js
@@ -0,0 +1,2 @@
+import{g as r,r as s}from"./index-B-gCgYSr.js";var o=s();const a=r(o);export{a as b};
+//# sourceMappingURL=index-fXNRe4Cl.js.map
